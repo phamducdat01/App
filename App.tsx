b58a7657a9596b96fcf661501c479ffebbbecf02
PHAMDUCDAT\phamd <phamducdat171102dta@gmail.com>
2024-03-01 14:39:13 +0700

    testJWT

--- App.tsx
+++ App.tsx
@@ -3,10 +3,16 @@ import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 import AuthNavigator from './src/navigator/AuthNavigator';
 import { SplashSreen } from './src/screens';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+import MainNavigator from './src/navigator/MainNavigator';
 
 const App = () => {
 
   const [isShowSplash, setisShowSplash] = useState(true);
+
+  const [accessToken, setaccessToken] = useState('');
+
+  const {getItem,setItem}=useAsyncStorage('assetToken');
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -16,11 +22,25 @@ const App = () => {
     return () => clearTimeout(timeout);
   }, []);
 
+  useEffect(()=>{
+    checkLogin();
+  },[]);
+
+  const checkLogin = async()=>{
+    const token =await getItem();
+    
+    token && setaccessToken(token);
+  }
+
   return (
     <>
       <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent />
-      {isShowSplash ? <SplashSreen /> : <NavigationContainer>
-        <AuthNavigator />
+      {isShowSplash ? <SplashSreen /> : 
+      <NavigationContainer>
+        {
+          accessToken? <MainNavigator />: <AuthNavigator />
+        }
+        
       </NavigationContainer>}
     </>
   )
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { appColors } from '../../constants/appColors'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems:'center'
+    }}>
+      <Text style={{color:appColors.primary}}>HomeScreen</Text>
+      <Button title='Logout' onPress={async ()=> await AsyncStorage.clear()} />
     </View>
   )
 }
