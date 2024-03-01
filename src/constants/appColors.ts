4a45460a762f66b74f6ce807e16f9efc1153ee5a
PHAMDUCDAT\phamd <phamducdat171102dta@gmail.com>
2024-03-01 13:12:09 +0700

    tow

---
+++ src/constants/appColors.ts
@@ -0,0 +1,7 @@
+export const appColors={
+    primary:'#5669FF',
+    white:'#fff',
+    text:'#37364A',
+    gay:'#9F9B9B',
+    gay2:'#A8A3A3'  
+}
---
+++ src/screens/index.ts
@@ -0,0 +1,5 @@
+import SplashSreen from "./SplashSreen";
+import LoginScreen from "./auth/LoginScreen";
+import HomeScreen from "./home/HomeScreen";
+
+export {HomeScreen,LoginScreen,SplashSreen};
