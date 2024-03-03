d1298a5d83010162a431c747c84d6077161b4916
PHAMDUCDAT\phamd <phamducdat171102dta@gmail.com>
2024-03-03 19:06:38 +0700

    add Onboarding

--- src/navigator/AuthNavigator.tsx
+++ src/navigator/AuthNavigator.tsx
@@ -1,13 +1,15 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { LoginScreen } from '../screens';
+import OnboardingScreen from '../screens/auth/OnboardingScreen';
 
 const AuthNavigator = () => {
 
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
   return (
-    <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name='LoginScreen' component={LoginScreen} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='OnboardingScreen' component={OnboardingScreen} />
+      <Stack.Screen name='LoginScreen' component={LoginScreen} />
     </Stack.Navigator>
   )
 }
---
+++ src/screens/auth/OnboardingScreen.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import { View, Image, TouchableOpacity, Text, StyleSheet } from 'react-native'
+import Swiper from 'react-native-swiper'
+import { appInfors } from '../../constants/appInfos'
+import { globalStyles } from '../../styles/globalStyles'
+import { appColors } from '../../constants/appColors'
+
+
+const OnboardingScreen = ({ navigation }: any) => {
+
+    const [index, setIndex] = useState(0);
+
+    return (
+        <View style={[globalStyles.container]}>
+            <Swiper style={{}}
+                loop={false}
+                onIndexChanged={num => { setIndex(num) }}
+                index={index}
+                activeDotColor={appColors.white}
+            >
+                <Image
+                    source={require('../../assets/img/onboarding-1.png')}
+                    style={{
+                        flex: 1,
+                        width: appInfors.sizes.WIDTH,
+                        height: appInfors.sizes.HEIGHT,
+                        resizeMode: 'cover'
+                    }}
+                />
+                <Image
+                    source={require('../../assets/img/onboarding-2.png')}
+                    style={{
+                        flex: 1,
+                        width: appInfors.sizes.WIDTH,
+                        height: appInfors.sizes.HEIGHT,
+                        resizeMode: 'cover'
+                    }}
+                />
+                <Image
+                    source={require('../../assets/img/onboarding-3.png')}
+                    style={{
+                        flex: 1,
+                        width: appInfors.sizes.WIDTH,
+                        height: appInfors.sizes.HEIGHT,
+                        resizeMode: 'cover'
+                    }}
+                />
+
+            </Swiper>
+
+            <View style={[
+                {
+                    paddingHorizontal: 20,
+                    paddingVertical: 20,
+                    position: 'absolute',
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }
+            ]}>
+                <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
+                    <Text style={[styles.text]}>Skip</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {
+                    return index < 2 ? setIndex(index + 1) : navigation.navigate('LoginScreen')
+                }}>
+                    <Text style={[styles.text]}>Next</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+export default OnboardingScreen
+
+const styles = StyleSheet.create({
+    text: {
+        color: appColors.white,
+        fontSize:16,
+        fontWeight:'500',
+    }
+})
