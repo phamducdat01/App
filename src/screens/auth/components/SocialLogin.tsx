034eb6ec4a9744496bf0cccadccdf0780407afec
phamducdat <phamducdat171102dta@gmail.com>
2024-03-21 16:08:07 +0700

    add svg1

--- src/screens/auth/components/SocialLogin.tsx
+++ src/screens/auth/components/SocialLogin.tsx
@@ -11,7 +11,7 @@ const SocialLogin = () => {
             <TextComponent
                 styles={{ textAlign: 'center' }}
                 text='OR'
-                color={appColors.white}
+                color={appColors.gay}
                 size={16}
                 font={fontFamily.medium}
             />
